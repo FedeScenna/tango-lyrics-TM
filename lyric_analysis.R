08eac85e861a06378384136fbd21910e1b91b7d3
Federico Scenna <federico.scenna@gmail.com>
2017-11-12 20:55:40 -0300

    Editing analysis code

--- lyric_analysis.R
+++ lyric_analysis.R
@@ -1,5 +1,9 @@
 
+library(tm)
 library(tidyverse)
-
-
-
+data <- read.csv("tango_lyrics.csv", sep=";")
+lyrics<-Corpus(VectorSource(data$lyric))
+lyrics <- tm_map(lyrics, removeNumbers)
+lyrics <- tm_map(lyrics, removePunctuation)
+lyrics_dtm <- DocumentTermMatrix(lyrics)
+freq<- colSums(as.matrix(lyrics_dtm))
